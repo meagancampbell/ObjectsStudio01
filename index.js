923150ae3917f817bf10979f97d20d40fd4a69c8
Meagan Campbell <meagancampbell@users.noreply.github.com>
2021-06-23 01:42:01 +0000

    MC

--- index.js
+++ index.js
@@ -1,34 +1,40 @@
 // */ Code your selectRandomEntry function here:/*
-
-function selectRandomEntry(array){
-   let index = Math.floor(Math.random()*array.length);
-   return array[index];
-}
-
-// Code your buildCrewArray function here:
-
 let idNumbers = [291, 414, 503, 599, 796, 890];
 let newCrew = []; 
 
-function buildCrewArray(ids, candidates){ 
+function selectRandomEntry(idNumbers){
+   let index = Math.floor(Math.random()*idNumbers.length);
+   return idNumbers[index];
+}
 
-  let threeCandidates=[]
-  while(threeCandidates.length<3){
-  let item = selectRandomEntry(ids);
-  if(!threeCandidates.includes(item)){
-  threeCandidates.push(item);
+console.log(selectRandomEntry(idNumbers));
+let crewIDs = [];
+
+while (crewIDs.length < 3){
+  let randomEntry = selectRandomEntry(idNumbers);
+  
+  if (crewIDs.includes(randomEntry) === false) {
+    crewIDs.push(randomEntry)
   }
-} for (i=0; i < threeCandidates.length; i++){ 
-    for (j=0; j<candidates.length; j++){
-    if (candidates[j].astronautID === threeCandidates[i])
-       { newCrew.push(candidates[j]); 
-       } 
-     } 
-       } 
-       return newCrew;
-} 
+}
 
-// Code your buildCrewArray function here:
+// crewIDs.push(selectRandomEntry(idNumbers));
+console.log(crewIDs);
+
+//Code your buildCrewArray function here:
+
+function buildCrewArray(crewIDs, candidateObjects){
+  let crewCandidates=[];
+  for (let i= 0; i < crewIDs.length; i++){
+    for (let j = 0; j < candidateObjects.length; i++){
+     if (cadidateObjects[j].astronautID === crewIDs[i]){
+       crewCandidates.push(candidatesObjects[j].name);
+     } 
+    }  
+  }
+}
+
+//Code your buildCrewArray function here:
 
 let animals = [candidateA,candidateB,candidateC,candidateD,candidateE,candidateF];
 
@@ -82,5 +88,23 @@ let candidateF = {
 
 
 // Code your template literal and console.log statements:
-buildCrewArray(idNumbers,animals)
-console.log(`${newCrew[0].name},${newCrew[1].name}, and ${newCrew[2 ].name} are going to space!`);
+// buildCrewArray(idNumbers,animals)
+// console.log(`${newCrew[0].name},${newCrew[1].name}, and ${newCrew[2 ].name} are going to space!`);
+
+function getOrbitCircumfrence(radius = 2000){   
+  let circumference =  2 * Math.PI * radius;
+  circumference = * 1000
+}
+console.log(getOrbitCircumfrence());
+
+function getMissionDuration (numberOfOrbits, radius = 2000, speed = 28000 ){
+  let distance = numberOfOrbits * getOrbitCircumfrence(radius);
+  let time = distance / speed;
+  time = time.toFixed(2); // converts the number into a string
+  return time;
+  //time = Math.round(time * 100) / 100;  //keeps the number a number
+}
+
+console.log(getMissionDuration(5))  // expected = 2.24
+
+// space walk formula
